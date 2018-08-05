416cbf237ac884f3f2d9809cd361d07991b59099
Subhas Ch Bera <subhas.9874@gmail.com>
2018-08-05 09:46:33 +0530

    about me

--- ex5.py
+++ ex5.py
@@ -1,15 +1,15 @@
 # Storing variables in a string
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
+my_name = 'Subhas Chandra Bera'
+my_age = 31 # not a lie
+my_height = 176 # cm
+my_weight = 64 # kg
+my_eyes = 'Black'
 my_teeth = 'White'
-my_hair = 'Brown'
+my_hair = 'Black'
 
 print(f"Let's talk abut {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
+print(f"He's {my_height} cms tall.")
+print(f"He's {my_weight} kgs heavy.")
 print("Actually that's not too heavy")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
