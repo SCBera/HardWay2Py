7fec483fe0c003341e8fe660051464c368001ccd
Subhas Ch Bera <subhas.9874@gmail.com>
2018-08-08 07:32:05 +0530

    reading and writing in a file

--- ex15.py
+++ ex15.py
@@ -2,13 +2,14 @@ from sys import argv
 
 script, filename = argv
 
-txt = open(filename)
+txt = open(filename) #opens the file given in filename and
+# saves in variable txt.
 
 print(f"Here's your file {filename}:")
-print(txt.read())
+print(txt.read()) #reads and prints the content given in the file
 
 print("Type the filename again:")
-file_again = input("> ")
+file_again = input("> ") # can take another/same file as input
 
 txt_again = open(file_again)
 
