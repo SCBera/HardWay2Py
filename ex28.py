31b6385528627a48882a0adef15443d7c0b57e5c
Subhas Ch Bera <subhas.9874@gmail.com>
2018-08-10 07:40:02 +0530

    Boolean logic

---
+++ ex28.py
@@ -0,0 +1,10 @@
+print("True and True:True/", True and True) # True
+print("False and True: False/", False and True) #False
+print("1 == 1 and 2 == 1: False/", 1 == 1 and 2 == 1) #False
+print("\"test\" == \"test\": True/", "test" == "test") # True
+print("1 == 1 or 2 != 1: True/", 1 == 1 or 2 != 1)
+print("True and 1 == 1: True/", True and 1 == 1)
+print("False and 0 != 0: False/", False and 0 != 0)
+
+#result = 3 == 3 and (not("testing" == "testing" or "Python" == "Fun"))
+print("3 == 3 and (not(\"testing\" == \"testing\" or \"Python\" == \"Fun\")): False/", 3 == 3 and (not("testing" == "testing" or "Python" == "Fun")))
