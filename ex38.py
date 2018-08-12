df7290a3ff1517bc452ad43d86ab80dcbe3cbe6e
Subhas Ch Bera <subhas.9874@gmail.com>
2018-08-12 20:34:29 +0530

    list_manipulation

---
+++ ex38.py
@@ -0,0 +1,23 @@
+ten_things = "Apples Orages Cows Telephone Light Suger"
+
+print("Wait, there are not 10 things in that list. Let's fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee",
+              "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print("Adding: ", next_one)
+    stuff.append(next_one)
+    print(f"There are {len(stuff)} items now.")
+
+print("There we go: ", stuff)
+
+print("Let's do some things with stuff.")
+
+print(stuff[1])  # prints 2nd (0, 1) element from a list
+print(stuff[-1])  # prints last element from a list
+print(stuff.pop())
+print(' '.join(stuff))  # joins elements from a list with space
+print('#'.join(stuff[3:5]))  # joins 4th and 6th elements with #
