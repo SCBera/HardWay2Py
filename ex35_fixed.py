6e6a1f87e489bdd203fee04cb6af80e07084eb4e
Subhas Ch Bera <subhas.9874@gmail.com>
2018-08-12 18:46:51 +0530

    case-insensitive-input

--- ex35_fixed.py
+++ ex35_fixed.py
@@ -1,15 +1,16 @@
 from sys import exit
 
+
 def gold_room():
 	print("This room is full of gold. How much do you take?")
-	
+
 	choice = input("> ")
-	
-	if choice in ["all", "All", "Everything", "everything"]:
+
+	if choice.lower() in ["all", "everything", "too much"]:
 		dead("You are greedy bastard!")
 	else:
 		try:
-			how_much = float(choice) # only work for number input
+			how_much = float(choice)  # only work for number input
 			if how_much < 50.0:
 				print("Nice, you're not greedy, you win!")
 				exit(0)
@@ -17,6 +18,7 @@ def gold_room():
 				dead("You greedy bastard!")
 		except:
 			dead("Man, learn to type properly.")
+
 
 def bear_room():
     print("There is a bear here.")
@@ -26,9 +28,11 @@ def bear_room():
     bear_moved = False
 
     while True:
-        choice = input("(take honey/taunt bear/...)> ")
+        choice = input("(You can try to: take honey/taunt bear/open the door/...)> ")
 
-        if choice == "take honey":
+        if choice == "open the door" and not bear_moved:
+            print("You need to move the bear to open the door!")
+        elif choice == "take honey":
             dead("The bear looks at you then slaps your face off.")
         elif choice == "taunt bear" and not bear_moved:
             print("The bear has moved from the door.")
@@ -41,6 +45,7 @@ def bear_room():
         else:
             print("I got no idea what that means.")
 
+
 def cthulhu_room():
     print("Here you see the great evil Cthulhu.")
     print("He, it, whatever stares at you and you go insane.")
@@ -50,15 +55,17 @@ def cthulhu_room():
 
     if "flee" in choice:
         print("You can't flee from Cthulhu's room")
-        start() # goes to the bottom of the code
+        start()  # goes to the bottom of the code
     elif "head" in choice:
         dead("Well that was tasty!")
     else:
         cthulhu_room()
 
+
 def dead(why):
     print(why, "Good try! Better luck next time!")
     exit(0)
+
 
 def start():
     print("You are in dark room.")
