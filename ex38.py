e52c040c683dbc84d4f2ef4d8b6a9e7af415b8e8
Subhas Ch Bera <subhas.9874@gmail.com>
2018-08-14 00:00:36 +0530

    teaking

--- ex38.py
+++ ex38.py
@@ -1,5 +1,6 @@
 ten_things = "Apples Orages Cows Telephone Light Suger"
 
+print("Things:", ten_things)
 print("Wait, there are not 10 things in that list. Let's fix that.")
 
 stuff = ten_things.split(' ')
@@ -7,6 +8,7 @@ more_stuff = ["Day", "Night", "Song", "Frisbee",
               "Corn", "Banana", "Girl", "Boy"]
 
 while len(stuff) != 10:
+    # for len(stuff) < 10:
     next_one = more_stuff.pop()
     print("Adding: ", next_one)
     stuff.append(next_one)
@@ -16,8 +18,8 @@ print("There we go: ", stuff)
 
 print("Let's do some things with stuff.")
 
-print(stuff[1])  # prints 2nd (0, 1) element from a list
-print(stuff[-1])  # prints last element from a list
-print(stuff.pop())
-print(' '.join(stuff))  # joins elements from a list with space
+print("The 2nd element:", stuff[1])  # prints 2nd (0, 1) element from a list
+print("The last element:", stuff[-1])  # prints last element from a list
+print("pop:", stuff.pop(), stuff)
+print("join stuff:", ' '.join(stuff))  # joins elements from a list with space
 print('#'.join(stuff[3:5]))  # joins 4th and 6th elements with #
